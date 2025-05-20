e3d273a5b3ce8b4b79665704631ac0c39f6f1768
aditya <adityavittal92@gmail.com>
2025-05-20 23:20:39 +0530

    instance create and update

--- mongodb.sh
+++ mongodb.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 USERID=$(id -u)
-R="/e[31m"
-G="/e[32m"
-B="/e[34m"
-Y="/e[33m"
-M="/e[35m"
-C="/e[36m"
-N="/e[0m"
+R="\e[31m"
+G="\e[32m"
+B="\e[34m"
+Y="\e[33m"
+M="\e[35m"
+C="\e[36m"
+N="\e[0m"
 LOGS_FOLDER="/var/log/roboshop-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 SCRIPT_DIR=$PWD
+
 
 
 echo "Script started executing at $(date)" | tee -a $LOG_FILE
--- shipping.sh
+++ shipping.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 USERID=$(id -u)
-R="/e[31m"
-G="/e[32m"
-B="/e[34m"
-Y="/e[33m"
-M="/e[35m"
-C="/e[36m"
-N="/e[0m"
+R="\e[31m"
+G="\e[32m"
+B="\e[34m"
+Y="\e[33m"
+M="\e[35m"
+C="\e[36m"
+N="\e[0m"
 LOGS_FOLDER="/var/log/roboshop-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
