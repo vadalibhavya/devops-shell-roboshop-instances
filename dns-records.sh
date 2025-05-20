747234f85f0547c75c6ad61211dcb3e11bb92af1
aditya <adityavittal92@gmail.com>
2025-05-20 20:06:08 +0530

    instance create and update

--- dns-records.sh
+++ dns-records.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
+ZONE_ID="Z05489693LFV4727Y7R4T"
 instances=("frontend" "mongodb" "catalogue" "redis" "user" "cart" "shipping" "payment" "dispatch" "mysql" "rabbitmq")
-
-
 # create/update the dns records for the instances with public ip to be instance-internal/instance-public my domain is doubtfree.online
 echo "creating private and public dns records if not available for instances, else updating the dns records"
   aws route53 change-resource-record-sets \
@@ -14,6 +13,4 @@ echo "creating private and public dns records if not available for instances, el
     --change-batch '{"Changes":[{"Action":"UPSERT","ResourceRecordSet":{"Name":"'$instance'-private.doubtfree.online","Type":"A","TTL":300,"ResourceRecords":[{"Value":"'$PRIVATE_IP'"}]}}]}' \
     --output text
   echo "Private DNS record created/updated for $instance"
-
-
 echo "All instances created and dns records updated"
