de51732e89165c24049dffce56b89a58595bcbde
aditya <adityavittal92@gmail.com>
2025-05-20 22:57:42 +0530

    instance create and update

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# List of instance names
+instances=("frontend" "mongodb" "catalogue" "cart" "user" "shipping" "payment" "dispatch" "redis" "mysql" "rabbitmq")
+
+# Replace with your actual values
+PASSWORD="DevOps321"
+USER="ec2-user"
+
+for instance in "${instances[@]}"
+do
+  echo "Processing $instance"
+
+  sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no "$USER@$instance-latest" 'bash -s' <<EOF
+    cd /home/ec2-user/devops-shell-roboshop-instances
+    bash $instance.sh
+EOF
+
+done
+
+echo "All instances have been processed."
