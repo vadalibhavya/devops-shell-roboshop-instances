17c6756230fa34e3d622e1acdc9d273a2e87165b
aditya <adityavittal92@gmail.com>
2025-05-20 20:07:18 +0530

    instance create and update

--- dns-records.sh
+++ dns-records.sh
@@ -3,6 +3,17 @@ ZONE_ID="Z05489693LFV4727Y7R4T"
 instances=("frontend" "mongodb" "catalogue" "redis" "user" "cart" "shipping" "payment" "dispatch" "mysql" "rabbitmq")
 # create/update the dns records for the instances with public ip to be instance-internal/instance-public my domain is doubtfree.online
 echo "creating private and public dns records if not available for instances, else updating the dns records"
+
+for instance in "${instances[@]}"
+do
+  PUBLIC_IP=$(aws ec2 describe-instances \
+    --filters "Name=tag:Name,Values=$instance" \
+    --query "Reservations[0].Instances[0].PublicIpAddress" \
+    --output text)
+  PRIVATE_IP=$(aws ec2 describe-instances \
+    --filters "Name=tag:Name,Values=$instance" \
+    --query "Reservations[0].Instances[0].PrivateIpAddress" \
+    --output text)
   aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
     --change-batch '{"Changes":[{"Action":"UPSERT","ResourceRecordSet":{"Name":"'$instance'.doubtfree.online","Type":"A","TTL":300,"ResourceRecords":[{"Value":"'$PUBLIC_IP'"}]}}]}' \
@@ -13,4 +24,5 @@ echo "creating private and public dns records if not available for instances, el
     --change-batch '{"Changes":[{"Action":"UPSERT","ResourceRecordSet":{"Name":"'$instance'-private.doubtfree.online","Type":"A","TTL":300,"ResourceRecords":[{"Value":"'$PRIVATE_IP'"}]}}]}' \
     --output text
   echo "Private DNS record created/updated for $instance"
+done
 echo "All instances created and dns records updated"
