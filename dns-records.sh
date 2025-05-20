7d5ef078d7bf778946aa179213eda63fe68a0c47
aditya <adityavittal92@gmail.com>
2025-05-20 20:58:27 +0530

    instance create and update

--- dns-records.sh
+++ dns-records.sh
@@ -6,35 +6,15 @@ instances=$(aws ec2 describe-instances \
   --query "Reservations[*].Instances[*].{InstanceId:InstanceId,Name:Tags[?Key=='Name']|[0].Value}" \
   --output text)
 
-
-for instance in $instances
+for instance in "${instances[@]}"
 do
-  PUBLIC_IP=$(aws ec2 describe-instances \
-    --filters "Name=tag:Name,Values=$instance" \
-    --query "Reservations[0].Instances[0].PublicIpAddress" \
-    --output text)
-  PRIVATE_IP=$(aws ec2 describe-instances \
-    --filters "Name=tag:Name,Values=$instance" \
-    --query "Reservations[0].Instances[0].PrivateIpAddress" \
-    --output text)
-
-  if [[ -n "$PUBLIC_IP" && "$PUBLIC_IP" != "None" ]]; then
-    aws route53 change-resource-record-sets \
-      --hosted-zone-id $ZONE_ID \
-      --change-batch '{"Changes":[{"Action":"UPSERT","ResourceRecordSet":{"Name":"'$instance'.doubtfree.online","Type":"A","TTL":300,"ResourceRecords":[{"Value":"'$PUBLIC_IP'"}]}}]}' \
-      --output text
-    echo "Public DNS record created/updated for $instance"
-  else
-    echo "Skipping public DNS update for $instance: No valid public IP"
-  fi
-
-  if [[ -n "$PRIVATE_IP" && "$PRIVATE_IP" != "None" ]]; then
-    aws route53 change-resource-record-sets \
-      --hosted-zone-id $ZONE_ID \
-      --change-batch '{"Changes":[{"Action":"UPSERT","ResourceRecordSet":{"Name":"'$instance-internal'.doubtfree.online","Type":"A","TTL":300,"ResourceRecords":[{"Value":"'$PRIVATE_IP'"}]}}]}' \
-      --output text
-    echo "Private DNS record ccreated/updated for $instance"
-  else
-    echo "Skipping private DNS update for $instance: No valid private IP"
-  fi
+  echo "Processing $instance"
+  aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '{"Changes":[{"Action":"UPSERT","ResourceRecordSet":{"Name":"'$instance'.doubtfree.online","Type":"A","TTL":300,"ResourceRecords":[{"Value":"'$PUBLIC_IP'"}]}}]}' \
+    --output text
+  aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '{"Changes":[{"Action":"UPSERT","ResourceRecordSet":{"Name":"'$instance-internal'.doubtfree.online","Type":"A","TTL":300,"ResourceRecords":[{"Value":"'$PRIVATE_IP'"}]}}]}' \
+    --output text
 done
