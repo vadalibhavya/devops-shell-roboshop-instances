79de6c283c43ad20314d588fc5d3597eeffc0377
aditya <adityavittal92@gmail.com>
2025-05-21 01:24:57 +0530

    instance create and update

--- frontend.sh
+++ frontend.sh
@@ -68,7 +68,7 @@ VALIDATE $? "Extracting frontend content"
 echo "Configuring frontend"
 rm -rf /etc/nginx/nginx.conf &>> $LOG_FILE
 # over write the existing nginx.conf
-cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf &>> $LOG_FILE
+cp nginx.conf /etc/nginx/nginx.conf &>> $LOG_FILE
 VALIDATE $? "Configuring frontend"
 
 echo "Restarting nginx"
