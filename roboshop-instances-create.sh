aaf4d438d337972734346114b2ef79614c739ac6
aditya <adityavittal92@gmail.com>
2025-05-20 22:47:46 +0530

    instance create and update

--- roboshop-instances-create.sh
+++ roboshop-instances-create.sh
@@ -7,32 +7,44 @@ ZONE_ID="Z05489693LFV4727Y7R4T"
 
 instances=("frontend" "mongodb" "catalogue" "redis" "user" "cart" "shipping" "payment" "dispatch" "mysql" "rabbitmq")
 
-# Checking if AWS CLI is installed
-if aws --version &> /dev/null; then
-  echo "AWS CLI is installed"
-else
+# Check and install AWS CLI
+if ! aws --version &> /dev/null; then
   echo "AWS CLI is NOT installed"
   dnf install awscli -y
-  echo "AWS CLI installed successfully"
+else
+  echo "AWS CLI is installed"
 fi
 
-if git --version &> /dev/null; then
-  echo "Git is installed"
-else
+# Check and install Git
+if ! git --version &> /dev/null; then
   echo "Git is NOT installed"
   dnf install git -y
-  echo "Git installed successfully"
+else
+  echo "Git is installed"
 fi
+
 git clone https://github.com/roboshop-devops-projects/devops-shell-roboshop-instances.git
 
 for instance in "${instances[@]}"; do
   echo "Creating $instance instance"
 
+  # Create user data script for EC2 instance
+  USER_DATA=$(base64 <<EOF
+#!/bin/bash
+dnf install git -y
+git clone https://github.com/roboshop-devops-projects/devops-shell-roboshop-instances.git
+cd devops-shell-roboshop-instances
+bash $instance.sh
+EOF
+)
+
+  # Launch EC2 with user data
   INSTANCE_ID=$(aws ec2 run-instances \
     --image-id "$AMI_ID" \
     --instance-type "$INSTANCE_TYPE" \
     --security-group-ids "$SECURITY_GROUP_ID" \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance-latest},{Key=service,Value=$instance}]" \
+    --user-data "$USER_DATA" \
     --query "Instances[0].InstanceId" \
     --output text)
 
